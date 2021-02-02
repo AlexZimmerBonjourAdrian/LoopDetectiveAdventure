973d9cd24f3c8ef72de180d6c84f79281d492bf3
Alex <alexzimer.zimmer1@gmail.com>
2021-02-02 13:12:10 -0300

    Respaldo Characters

--- Assets/Scripts/Objects/CCharacter.cs
+++ Assets/Scripts/Objects/CCharacter.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Subtegral.DialogueSystem.DataContainers;
 
 //[SerializeField]
 public class CCharacter : CGenericCharacter , Iinteract
 {
     
     // Start is called before the first frame update
-   private new string nameCharacter= " ";
-    
+   [SerializeField]
+    private new string nameCharacter= " ";
+    [SerializeField]
+    private DialogueContainer dialogueContainer;
+    [SerializeField]
+    private CPlayerData playerData;
+
+
     // Update is called once per frame
     public override void Oninteract()
     {
